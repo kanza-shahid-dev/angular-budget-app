37fe205768981b9ce95f5e4660fc99e431a79650
Kanza Shahid <kanza.shahid@teo-intl.com>
2024-05-22 16:37:10 +0500

    register/login feature

--- src/app/budget-planner/login/login.component.ts
+++ src/app/budget-planner/login/login.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: any;
+  registerForm: any;
+  activeForm: 'login' | 'register' = 'login';
 
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+  login() {
+    if (this.loginForm.valid) {
+      alert('valid');
+      this.router.navigate(['/budget-planner/dashboard']);
+    }
+  }
+  register() {
+    if (this.registerForm.valid) {
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+      this.router.navigate(['/budget-planner/login']);
+    } else {
+      alert('Invalid Form');
+    }
+  }
+  toggleForm(value: 'login' | 'register') {
+    this.activeForm = value;
+  }
 }
